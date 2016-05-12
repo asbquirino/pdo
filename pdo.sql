9ad79a1beb3411fbab01c6b075115ed43646b640
Antonio Sergio <antonio.quirino@campinas.sp.gov.br>
2016-05-12 14:38:29 -0300

    Primeiro commit.

---
+++ pdo.sql
@@ -0,0 +1,28 @@
+create database pdo;
+
+create table alunos(
+  id INT AUTO_INCREMENT Primary Key,
+  nome VARCHAR(255),
+  nota INT);
+
+insert into alunos(nome,nota) values('Antonio',10);
+insert into alunos(nome,nota) values('Sergio',9);
+insert into alunos(nome,nota) values('Marcia',8);
+insert into alunos(nome,nota) values('Regina',7);
+insert into alunos(nome,nota) values('Maria',6);
+insert into alunos(nome,nota) values('Aline',5);
+insert into alunos(nome,nota) values('Gustavo',4);
+insert into alunos(nome,nota) values('Carlos',2);
+insert into alunos(nome,nota) values('Aparecida',1);
+insert into alunos(nome,nota) values('Alcides',0);
+insert into alunos(nome,nota) values('Gabriel',9);
+insert into alunos(nome,nota) values('Tiago',8);
+insert into alunos(nome,nota) values('Diego',7);
+insert into alunos(nome,nota) values('Silvia',6);
+insert into alunos(nome,nota) values('Cleber',5);
+insert into alunos(nome,nota) values('Airton',4);
+insert into alunos(nome,nota) values('Miriam',3);
+insert into alunos(nome,nota) values('Roberto',3);
+insert into alunos(nome,nota) values('Eunice',2);
+insert into alunos(nome,nota) values('Cristina',1);
+
