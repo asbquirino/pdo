c2157ee485035250855081e32350a23465e8d437
Antonio Sergio <asbquirino@yahoo.com.br>
2016-09-17 21:36:00 -0300

    alterações para atender o fase atual do projeto

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 
-require_once 'Cliente.php';
+require_once 'Aluno.php';
 
 try{
     $conexao = new \PDO("mysql:host=localhost;dbname=pdo","root","");
@@ -9,17 +9,30 @@ catch (\PDOException $e){
     die("Não foi possível estabelecar a conexão com o banco de dados. Erro: ".$e->getCode()." - ".$e->getMessage());
 }
 
-$cliente = new Cliente($conexao);
+$aluno = new Aluno($conexao);
+echo "<h1>Alunos</h1>";
+
+echo "<p></p></h3><a href='novoaluno.php'>Novo Aluno</a></p>";
+
+// --- Listar
+echo "<table cellpadding='5'>  
+          <tr>
+              <th colspan='2'>Ação</th>
+              <th>Nome</th>
+          </tr>";
+foreach($aluno->listar("nome") as $c) {
+    echo "<tr>
+              <td><a href='alteraaluno.php?id=".$c['id']."'>Altera</a></td>
+              <td><a href='excluialuno.php?id=".$c['id']."'>Exclui</a></td>
+              <td><a href='mostraaluno.php?id=".$c['id']."'>".$c['nome']."</a></td>
+          </tr>";
+}
+echo "</table>";
 
 // --- Incluir
 //$cliente->setNome("Wesley")
 //        ->setEmail("wesley@wesley.com.br")
 //;
-//foreach($cliente->listar("id desc") as $c) {
-//    echo $c['nome']."<br>";
-//}
-
-// --- Listar
 //foreach($cliente->listar("id desc") as $c) {
 //    echo $c['nome']."<br>";
 //}
@@ -39,6 +52,6 @@ $cliente = new Cliente($conexao);
 //    echo $c['nome']."<br>";
 //}
 
-# --- Find
-$resultado = $cliente->find(6);
-echo $resultado['nome']." - ".$resultado['email']."<br>";
+// --- Find
+//$resultado = $cliente->find(6);
+//echo $resultado['nome']." - ".$resultado['email']."<br>";
