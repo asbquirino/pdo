b41888656fb07f6e36bef5122627430dea6f06c1
Antonio Sergio <asbquirino@yahoo.com.br>
2016-11-14 23:40:59 -0200

    Alterações na rotina altera aluno

--- alteraaluno.php
+++ alteraaluno.php
@@ -1,13 +1,40 @@
 <?php
 
 require_once 'Aluno.php';
+require_once 'conecta.php';
 
-try{
-    $conexao = new \PDO("mysql:host=localhost;dbname=pdo","root","");
-}
-catch (\PDOException $e){
-    die("Não foi possível estabelecar a conexão com o banco de dados. Erro: ".$e->getCode()." - ".$e->getMessage());
-}
+$alunos = new Aluno($conexao);
 
-$aluno = new Aluno($conexao);
-echo "<h1>Alunos</h1>";
+$alunos->setId($_GET['id']);
+
+$aluno = $alunos->find($alunos->getId());
+
+$alunos->setNome($aluno['nome'])
+       ->setNota($aluno['nota']);
+?>
+
+<h1>Edita Alunos</h1>
+
+<form action="gravaluno.php?id=<?php echo $alunos->getId(); ?>" method="post">
+    <table>
+        <tr>
+            <td>
+                <label>Nome</label>
+            </td>
+            <td>
+                <input type="text" name="nome" value="<?php echo $alunos->getNome(); ?>" size="100" maxlength="150">
+            </td>
+        </tr>
+        <tr>
+            <td>
+                <label>Nota</label>
+            </td>
+            <td>
+                <input type="number" name="nota" value="<?php echo $alunos->getNota(); ?>" min="0" max="10">
+            </td>
+        </tr>
+    </table>
+    <br/>
+    <input type="submit" value="Grava">
+    <input type="button" value="Volta" onclick="window.location.href='index.php'">
+</form>
